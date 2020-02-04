2642409bc2aae02da4b635b320388f9d8803fdbe
TREY <hugh.a.lloyd@gmail.com>
2020-02-04 02:01:57 -0600

    debug post request, other than that reg form comp is finished so other forms should go relatively quick

--- src/components/registerPage.js
+++ src/components/registerPage.js
@@ -4,8 +4,15 @@ import * as Yup from 'yup';
 import axios from 'axios'
 
 
-const RegisterPage = ({}) => {
+const RegisterPage = ({values, errors, touched, status}) => {
 // set state for posting users
+// const [users, setUsers] = useState([]);
+// useEffect(()=>{
+//     console.log('status has changed', status);
+//     status && setUsers(users => 
+//        [...users, status])
+// },[status])
+
     return (
         <section className='register-section'>
             <Form>
@@ -14,15 +21,18 @@ const RegisterPage = ({}) => {
 
                 <label className='user-Reg'>Create Username
                     <Field type='text' name='username'></Field>
+                    {touched.username && errors.username && <p className='errors-reg'>{errors.username}</p>}
 
                 </label>
 
                 <label className='email-Reg'>Email
                     <Field type='email' name='email'></Field>
+                    {touched.email && errors.email && <p className='errors-reg'>{errors.email}</p>}
                 </label>
 
                 <label className='password-Reg'>Create Password
                     <Field type='password' name='password'></Field>
+                    {touched.password && errors.password && <p className='errors-reg'>{errors.password}</p>}
                 </label>
 
              </div>
@@ -32,12 +42,14 @@ const RegisterPage = ({}) => {
                 <p>User Type</p>
 
                 <label className='checkBox-Reg'>Donor
-                <Field type='checkbox' name='user-type'></Field>
+                <Field type='radio' name='radio' value='donor'></Field>
+                {touched.radio && errors.radio && <p className='errors-reg'>{errors.radio}</p>}
                 </label>
 
                 
                 <label className='checkBox-Reg'>Organization
-                <Field type='checkbox' name='user-type'></Field>
+                <Field type='radio' name='radio' value='organization'></Field>
+                {touched.radio && errors.radio && <p className='errors-reg'>{errors.radio}</p>}
                 </label>
 
             </div>
@@ -57,16 +69,36 @@ const RegisterPage = ({}) => {
 }
 
 const FormikRegister = withFormik({
-    mapPropsToValues({}){
+    mapPropsToValues({username,email,password,radio}){
         return {
-
+            username:'',
+            email:'',
+            password:'',
+            radio:'',
         }
-    }
+    },
 
 
-// validation schema
 
-// handle submit and post
+validationSchema: Yup.object().shape({
+username: Yup.string().required(),
+email: Yup.string().required(),
+password: Yup.string().min(8).required(),
+// radio: Yup.boolean().oneOf(['donor','organization']),
+radio:Yup.string().required(),
+
+}),
+
+
+handleSubmit(values,formikBag) {
+    console.log('submitting',values);
+    axios.post('https://bw-save-the-animals.herokuapp.com/auth/register')
+    .then(res => {
+        console.log('post is working', res)
+        // setStatus(res.data)
+    })
+    .catch(err => console.log(err.res))
+}
 
 
 })(RegisterPage)
