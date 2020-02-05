a69fba4853b0b3e1233b1acb5ddaec03bc386541
TREY <hugh.a.lloyd@gmail.com>
2020-02-05 02:23:08 -0600

    added some styling-found theme to follow

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Container } from '@material-ui/core';
+import styled from 'styled-components';
+
+const HomeDiv = styled.div `
+margin:10% 25%;
+background-color:rgb(37, 68, 95,0.5);
+box-shadow: inset 0 0 10px #000000;
+border-radius:10px;
+`
+
+const Pgraph = styled.p `
+font-size:1.3rem;
+
+padding:10%;
+`
 
 const HomePage = () => {
 
     return (
-        <div>
-            <p>Key Conservation is helping conservationists receive critical funding and increased global support through an app that provides real-time updates on day-to-day campaigns. We can stop global extinction if we utilize new technology and interact with supporters in ways that they want! </p>
-        </div>
+        <HomeDiv>
+            <Pgraph>Key Conservation is helping conservationists receive critical funding and increased global support through an app that provides real-time updates on day-to-day campaigns. We can stop global extinction if we utilize new technology and interact with supporters in ways that they want! </Pgraph>
+        </HomeDiv>
     )
 }
 
